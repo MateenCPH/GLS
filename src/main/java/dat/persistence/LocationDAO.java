9ba9cd1c5175a866e525c4eb68e4705d5f303855
MateenCPH <cph-mr645@cphbusiness.dk>
2024-09-06 11:54:50 +0200

    created DAOs

--- src/main/java/dat/persistence/LocationDAO.java
+++ src/main/java/dat/persistence/LocationDAO.java
@@ -1,4 +1,66 @@
 package dat.persistence;
 
-public class LocationDAO {
+import dat.entities.Location;
+import dat.entities.Shipment;
+import dat.exceptions.JpaException;
+import dat.entities.Package;
+import dat.enums.HibernateConfigState;
+import jakarta.persistence.*;
+
+public class LocationDAO implements iDAO<Location> {
+    private static LocationDAO instance;
+    private static EntityManagerFactory emf;
+
+
+    private LocationDAO() {
+    }
+
+    public static LocationDAO getInstance(HibernateConfigState state) {
+        if (instance == null) {
+            emf = HibernateConfig.getEntityManagerFactoryConfig(state, "gls");
+            instance = new LocationDAO();
+        }
+        return instance;
+    }
+
+    public static EntityManagerFactory getEmf() {
+        return emf;
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+
+    @Override
+    public Location create(Location location) {
+        return null;
+    }
+
+    @Override
+    public Location findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Location findByTrackingNumber(String trackingNumber) {
+        try (EntityManager em = emf.createEntityManager()){
+            TypedQuery<Location> query = em.createQuery("SELECT l FROM Location l WHERE l.trackingNumber = :trackingNumber", Location.class);
+            query.setParameter("trackingNumber", trackingNumber);
+            return query.getSingleResult();
+        } catch (Exception e) {
+            throw new JpaException("Error finding location: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Location update(Location location) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Location location) {
+        return false;
+    }
 }
--- src/main/java/dat/persistence/ShipmentDAO.java
+++ src/main/java/dat/persistence/ShipmentDAO.java
@@ -1,4 +1,59 @@
 package dat.persistence;
 
-public class ShipmentDAO {
+import dat.entities.Shipment;
+import dat.exceptions.JpaException;
+import dat.entities.Package;
+import dat.enums.HibernateConfigState;
+import jakarta.persistence.*;
+
+public class ShipmentDAO implements iDAO<Shipment> {
+    private static ShipmentDAO instance;
+    private static EntityManagerFactory emf;
+
+
+    private ShipmentDAO() {
+    }
+
+    public static ShipmentDAO getInstance(HibernateConfigState state) {
+        if (instance == null) {
+            emf = HibernateConfig.getEntityManagerFactoryConfig(state, "gls");
+            instance = new ShipmentDAO();
+        }
+        return instance;
+    }
+
+    public static EntityManagerFactory getEmf() {
+        return emf;
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+
+    @Override
+    public Shipment create(Shipment shipment) {
+        return null;
+    }
+
+    @Override
+    public Shipment findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Shipment findByTrackingNumber(String trackingNumber) {
+        return null;
+    }
+
+    @Override
+    public Shipment update(Shipment shipment) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Shipment shipment) {
+        return false;
+    }
 }
